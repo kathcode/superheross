2cd95bb468d107f1c053a41414563c4a93639d67
Kath_code <kathmene27@gmail.com>
2018-08-04 17:24:11 -0500

    Adding react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import List from './views/List/List';
+import Ranking from './views/Ranking/Ranking';
+
+render(
+  <Fragment>
+    <Router basename="/">
+      <Switch>
+        <Route
+          path="/"
+          exact
+          component={List}
+        />
+        <Route
+          path="/ranking"
+          exact
+          component={Ranking}
+        />
+      </Switch>
+    </Router>
+  </Fragment>,
+  document.getElementById('root')
+);
