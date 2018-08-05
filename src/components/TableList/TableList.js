a5c94dbd62f3600be74915cf05d8bf98cece1ee5
Kath_code <kathmene27@gmail.com>
2018-08-04 21:18:54 -0500

    Create the component for the table view

--- src/components/TableList/TableList.js
+++ src/components/TableList/TableList.js
@@ -6,8 +6,8 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 
 const StyledAvatar = styled(Avatar)`
-  width: 160px !important;
-  height: 160px !important;
+  width: 60px !important;
+  height: 60px !important;
   box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
 `;
 
@@ -33,7 +33,7 @@ const StyledContainerAvatar = styled.div`
   }
 `;
 
-class GridList extends Component {
+class TableList extends Component {
   render() {
     const {
       hero,
@@ -43,18 +43,20 @@ class GridList extends Component {
     } = this.props;
 
     return (
-      <div className="col-md-6 mb-3">
+      <div className="col-md-12 mb-3">
         <Card className="pt-3 pr-3 pl-3 pb-3">
           <div className="row row-eq-height d-flex align-items-center justify-content-center">
-            <StyledContainerAvatar className="col-lg-4">
+            <StyledContainerAvatar className="col-lg-3">
               <StyledAvatar
                 alt={hero.picture}
                 src={hero.picture || 'https://www.namepros.com/a/2018/05/106343_82907bfea9fe97e84861e2ee7c5b4f5b.png'}
               />
             </StyledContainerAvatar>
-            <div className="col-lg-8">
+            <div className="col-lg-7">
               <strong>{hero.name}</strong>
               <div className="mt-3">{hero.info === '-' ? 'No description added' : hero.info }</div>
+            </div>
+            <div className="col-md-2">
               <div className="text-rigth">
                 <StyledCardActions>
                   <IconButton  onClick={() => addRanking(position)} aria-label="Add to favorites">
@@ -71,4 +73,4 @@ class GridList extends Component {
   }
 }
 
-export default GridList;
+export default TableList;
